5ccd6b34a8c9ad73ec5ec396b3c3676f459ff973
Krishnavamsi P <krishnavamsi.p@MB-M-KRISHNAVMSI.local>
2025-11-03 22:37:22 +0530

    nested routes

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FirstRoutePage = () => {
+  return <div>FirstRoutePage</div>;
+};
+
+export default FirstRoutePage;
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SecondRoutePage = () => {
+  return <div>SecondRoutePage</div>;
+};
+
+export default SecondRoutePage;
